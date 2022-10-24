f549aaaed81338122bf8eb5a9e0ce6e881e32270
EWE Code <111604338+eatwanderexplore@users.noreply.github.com>
2022-10-25 11:23:01 +1300

    added breakfast and lunch to output

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-//array of dinner ideas
-let message = ['spaghetti nests', 'bird tongue lemon soup', 'chili', 'fish and rice', 'pizza', 'pineapple fried rice', 'rice-a-roni'];
+//arrays of meal ideas
+const breakfasts = ['cereal', 'oatmeal', 'french toast', 'pancakes', 'liege waffles'];
+const lunches = ['PB&J sandwiches', 'leftovers', 'nachos', 'tuna casserole', 'cobb salad'];
+const dinners = ['spaghetti nests', 'bird tongue lemon soup', 'chili', 'fish and rice', 'pizza', 'pineapple fried rice', 'rice-a-roni'];
 
 //output random dinner idea function
-function whatsForDinner()  {
-    let dinner = message[Math.floor(Math.random()*message.length)];
-    return dinner;
+function whatToEat()  {
+    let breakfast = breakfasts[Math.floor(Math.random()*breakfasts.length)];
+    let lunch = lunches[Math.floor(Math.random()*lunches.length)];
+    let dinner = dinners[Math.floor(Math.random()*dinners.length)];
+    return `Today's menu: ${breakfast} for breakfast, ${lunch} for lunch, and ${dinner} for dinner.`;
 }
 
 
 
 
-//print random dinner idea
-console.log("You should have " + whatsForDinner() + " for dinner.");
+//print random meal ideas
+console.log(whatToEat());
